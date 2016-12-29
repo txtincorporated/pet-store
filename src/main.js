48f53e98fe5baeb078d58092401f05f15b6475d2
Nathan P. Keene <npk@txtincorporated.com>
2016-12-28 22:28:04 -0800

    Pet Store LAB2 setup

--- src/main.js
+++ src/main.js
@@ -1,21 +1,41 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
-import resource from 'angular-resource';
 
+// 3d party imports
+import animate from 'angular-animate';
+import resource from 'angular-resource';
+import uiRouter from 'angular-ui-router';
+import defaultRoute from 'angular-ui-router-default';
+// needed for old-school $stateChanged events but must 
+// manually grab module from angular, since not exported
+import 'angular-ui-router/release/stateEvents';
+
+import dialog from 'ng-dialog';
+import 'ng-dialog/css/ngDialog.css';
+import 'ng-dialog/css/ngDialog-theme-default.css';
+
+// file imports
 import components from './components';
 import services from './services';
 import routes from './routes';
 import './scss/main.scss';
+import http from './http';
+import auth from './auth';
 
 const app = angular.module('myApp', [
   components,
   services,
   uiRouter,
-  resource
+  resource, 
+  animate,
+  defaultRoute,
+  angular.module('ui.router.state.events').name,
+  dialog
 
 ]);
 
+app.congig(http);
 app.config(routes);
+app.config(auth);
 
 const dev = 'https://pet-store-401.herokuapp.com/api/unauth';
 
