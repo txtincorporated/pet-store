6628275f0cd828cbb4910edb291c01e5cd110cd6
Nathan P. Keene <npk@txtincorporated.com>
2016-12-31 02:27:11 -0800

    Add pets now displaying on click in store detail

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,12 @@ export default function routes($stateProvider, $urlRouterProvider) {
       store: ['$transition$', 'storesService', (t, stores) => stores.get(t.params().id)]
     }, 
     
+    // views: {
+    //   addpet: {
+    //     component: 'storesStoreAddpet'
+    //   }
+    // },
+    
     component: 'storesStore'
     
   });
